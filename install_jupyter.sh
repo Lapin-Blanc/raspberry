40a7ba34d1cdf89504f4368b2b07177a0af481ff
Fabien Toune <github@lapin-blanc.com>
2019-12-03 10:07:02 +0100

    Update comments

--- install_jupyter.sh
+++ install_jupyter.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-
+# Toutes le commandes qui commencent par sudo s'exécutent en tant qu'administrateur
+# Consulte la liste des mises à jour disponibles
 sudo apt-get -y update
+# Effectue les mises à jour
 sudo apt-get -y upgrade
+# Installe le gestionnaire de modules python
 sudo apt-get -y install python3-pip
+# Installe le serveur Jupyter Lab
 sudo pip3 install jupyterlab
+# Génère le fichier de configuration de base de Jupyter Lab
 jupyter notebook --generate-config
+# Configure le mot de passe pour accéder au site
 jupyter notebook password
+# Configure le fichier de configuration pour que n'importe quelle machine sur le réseau puisse se connecter
 echo "c.NotebookApp.allow_origin = '*'" > /home/pi/.jupyter/jupyter_notebook_config.py
+# Configure le fichier de configuration pour que le serveur attende les connection sur l'adresse IP publique
 echo "c.NotebookApp.ip = '0.0.0.0'" > /home/pi/.jupyter/jupyter_notebook_config.py
+# Crée le dossier où seront enregistrés les notebooks
 mkdir /home/pi/Notebooks
+# Crée un script de démarrage (service) pour pouvoir lancer automatiquement le script au démarrage du système
 sudo echo "[Unit]
 Description=Jupyter Notebook
 
@@ -25,7 +35,9 @@ RestartSec=10
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/jupyter.service
-
+# Lance manuellement le service
 sudo systemctl start jupyter
+# Active le lancement automatique du service au démarrage du système
 systemctl enable jupyter
+# Vérifie l'état du service
 sudo systemctl status jupyter
